eb9908d53f2502c35af6dd33b9f9964163ba6415
gesim-dave123 <christiandavegesim@gmail.com>
2026-05-09 03:24:41 +0800

    Add delete modal state and handlers

--- src/app/profile/update.component.ts
+++ src/app/profile/update.component.ts
@@ -9,71 +9,86 @@ import { Account } from '@app/_models';
 
 @Component({ templateUrl: 'update.component.html', standalone: false })
 export class UpdateComponent implements OnInit {
-    account!: Account;
-    form!: FormGroup;
-    submitting = false;
-    submitted = false;
-    deleting = false;
+  account!: Account;
+  form!: FormGroup;
+  submitting = false;
+  submitted = false;
+  deleting = false;
+  showDeleteModal = false;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private accountService: AccountService,
-        private alertService: AlertService
-    ) { }
-    ngOnInit() {
-        this.account = this.accountService.accountValue!;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService,
+  ) {}
+  ngOnInit() {
+    this.account = this.accountService.accountValue!;
 
-        this.form = this.formBuilder.group({
-            title: [this.account.title, Validators.required],
-            firstName: [this.account.firstName, Validators.required],
-            lastName: [this.account.lastName, Validators.required],
-            email: [this.account.email, [Validators.required, Validators.email]],
-            password: ['', [Validators.minLength(6)]],
-            confirmPassword: ['']
-        }, {
-            validator: MustMatch('password', 'confirmPassword')
-        });
+    this.form = this.formBuilder.group(
+      {
+        title: [this.account.title, Validators.required],
+        firstName: [this.account.firstName, Validators.required],
+        lastName: [this.account.lastName, Validators.required],
+        email: [this.account.email, [Validators.required, Validators.email]],
+        password: ['', [Validators.minLength(6)]],
+        confirmPassword: [''],
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword'),
+      },
+    );
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.form.controls; }
+    this.submitting = true;
+    this.accountService
+      .update(this.account.id!, this.form.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.alertService.success('Update successful', { keepAfterRouteChange: true });
+          this.router.navigate(['../'], { relativeTo: this.route });
+        },
+        error: (error) => {
+          this.alertService.error(error);
+          this.submitting = false;
+        },
+      });
+  }
+  openDeleteModal() {
+    this.showDeleteModal = true;
+  }
 
-    onSubmit() {
-        this.submitted = true;
+  closeDeleteModal() {
+    this.showDeleteModal = false;
+  }
 
-        // reset alerts on submit
-        this.alertService.clear();
-
-        // stop here if form is invalid 
-        if (this.form.invalid) {
-            return;
-        }
-
-        this.submitting = true;
-        this.accountService.update(this.account.id!, this.form.value)
-            .pipe(first())
-            .subscribe({
-                next: () => {
-                    this.alertService.success('Update successful', { keepAfterRouteChange: true });
-                    this.router.navigate(['../'], { relativeTo: this.route });
-                },
-                error: error => {
-                    this.alertService.error(error);
-                    this.submitting = false;
-                }
-            });
-    }
-    onDelete() {
-        if (confirm('Are you sure?')) {
-            this.deleting = true;
-            this.accountService.delete(this.account.id!)
-                .pipe(first())
-                .subscribe(() => {
-                    this.alertService.success('Account deleted successfully', { keepAfterRouteChange: true });
-                });
-        }
-    }
+  confirmDelete() {
+    this.deleting = true;
+    this.accountService
+      .delete(this.account.id!)
+      .pipe(first())
+      .subscribe(() => {
+        this.alertService.success('Account deleted successfully', { keepAfterRouteChange: true });
+        this.deleting = false;
+        this.showDeleteModal = false;
+      });
+  }
 }
-
