80ab37ca3daeeef28bc1f22d128f7785aa7a7cfd
gesim-dave123 <christiandavegesim@gmail.com>
2026-05-09 03:24:40 +0800

    Import RouterModule for routerLink usage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [HomeComponent],
-    exports: [HomeComponent]
+  imports: [CommonModule, RouterModule],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
